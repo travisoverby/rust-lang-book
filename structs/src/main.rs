bbb97f2a624261651779e57e0bb24a52fb13923b
Travis Overby <travisoverby@gmail.com>
2024-02-03 17:43:11 -0800

    Chapter 5.3 defining methods end

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
 }
 
 fn main() {
@@ -20,4 +24,8 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
 }
