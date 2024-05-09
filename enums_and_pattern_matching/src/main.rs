3219221483eed642d7ab0500042a30231d6c287c
Travis Overby <travisoverby@gmail.com>
2024-05-09 16:27:39 -0700

    Chapter 6.1 enum and struct similarities

--- enums_and_pattern_matching/src/main.rs
+++ enums_and_pattern_matching/src/main.rs
@@ -1,9 +1,30 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
+/*
+ Above enum equivalent to below structs:
+
+ struct QuitMessage; // unit struct
+ struct MoveMessage {
+    x: i32,
+    y: i32,
+ }
+ struct WriteMessage(String); // tuple struct
+ struct ChangeColorMessage(i32, i32, i32); // tuple struct
+*/
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
+}
+
+
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));    
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
