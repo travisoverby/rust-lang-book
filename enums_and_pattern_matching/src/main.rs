f0d5b257058a1024b5709d622d43df1c2a9bd217
Travis Overby <travisoverby@gmail.com>
2024-05-29 21:40:45 -0700

    6.3 if-let control flow example 2

--- enums_and_pattern_matching/src/main.rs
+++ enums_and_pattern_matching/src/main.rs
@@ -1,12 +1,33 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    // ...etc
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
-    let config_max = Some(3u8);
+    let mut count = 0;
+    let coin = Coin::Nickel;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+    //     _ => count += 1,
+    // }
+    // println!("The current count is {}", count);
 
-    match config_max {
-        Some(max) => println!("The maximum is configured to be {}", max),
-        _ => (),
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
     }
 
-    if let Some(max) = config_max {
-        println!("The maximum is configured to be {}", max);
-    }
+    println!("The current count is {}", count);
 }
