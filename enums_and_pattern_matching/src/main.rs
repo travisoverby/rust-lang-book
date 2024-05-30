2ba201c5c248fb3737d72bbba976d37ebc6940be
Travis Overby <travisoverby@gmail.com>
2024-05-29 17:47:26 -0700

    6.2 exhaustive matches

--- enums_and_pattern_matching/src/main.rs
+++ enums_and_pattern_matching/src/main.rs
@@ -1,35 +1,12 @@
-#[derive(Debug)] // so we can inspect the state in a minute
-enum UsState {
-    Alabama,
-    Alaska,
-    Arizona,
-    Arkansas,
-    California,
-    // ...etc
-}
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        },
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        },
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
 fn main() {
-    let coin = Coin::Quarter(UsState::California);
-    value_in_cents(coin);
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
