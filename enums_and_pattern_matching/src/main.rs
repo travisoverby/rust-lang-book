c6f3aa5fd800d513151732d42bcc47325ac3288a
Travis Overby <travisoverby@gmail.com>
2024-05-29 17:57:54 -0700

    6.2 catch-all patterns and the _ placeholder

--- enums_and_pattern_matching/src/main.rs
+++ enums_and_pattern_matching/src/main.rs
@@ -1,12 +1,11 @@
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
-}
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
 
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+    }
 }
